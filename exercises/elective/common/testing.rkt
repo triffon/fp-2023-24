c1d04dce1871a94f3a814d83619bf24583be7d38
David Petrov <david.petrov@limechain.tech>
2023-10-24 19:29:50 +0300

    feat(exercises/elective)(testing)!: extract testing 'framework' logic

---
+++ exercises/elective/common/testing.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+;; Testing, don't look (yet :D)
+(require rackunit)
+(require rackunit/text-ui)
+
+(define-namespace-anchor anc)
+(define ns (namespace-anchor->namespace anc))
+
+(define-syntax (gen-test-suite stx)
+  (syntax-case stx ()
+    [(_ fn tests)
+     (with-syntax ([header (format "~a" (syntax->datum #'fn))])
+       #'(begin
+           (display
+            (string-append "\nTesting " header ":\n"))
+
+           (time
+            (eval
+             `(run-tests
+               (test-suite
+                ,header
+                ,@(map (lambda (test-params)
+                         (let ([args     (car test-params)]
+                               [expected (cdr test-params)])
+                           `(check equal? (,fn ,@args) ,expected)))
+                       tests)))
+             ns))
+           (values)))]))
+
+(provide (all-defined-out))
