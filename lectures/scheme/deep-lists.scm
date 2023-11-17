0a0cc216a7d0f634519a277d1331c0fe6e8004c3
Trifon Trifonov <triffon@gmail.com>
2023-11-17 22:48:06 +0200

    Функции от по-висок ред за дълбоки списъци

--- lectures/scheme/deep-lists.scm
+++ lectures/scheme/deep-lists.scm
@@ -7,7 +7,40 @@
         ((atom? dl) 1)
         (else (+ (count-atoms (car dl)) (count-atoms (cdr dl))))))
 
+(define (count-atoms dl)
+  (deep-foldr + 0 (lambda (x) 1) dl))
+
 (define (flatten dl)
   (cond ((null? dl) '())
         ((atom? dl) (list dl))
         (else (append (flatten (car dl)) (flatten (cdr dl))))))
+
+(define (flatten dl)
+  (deep-foldr append '() list dl))
+
+(define (snoc x r) (append r (list x)))
+
+(define (deep-reverse dl)
+  (cond ((null? dl) '())
+        ((atom? dl) dl)
+        (else (snoc (deep-reverse (car dl)) (deep-reverse (cdr dl))))))
+
+(define (id x) x)
+
+(define (deep-reverse dl)
+  (deep-foldr snoc '() id dl))
+
+(define (deep-foldr op nv term dl)
+  (cond ((null? dl) nv)
+        ((atom? dl) (term dl))
+        (else (op (deep-foldr op nv term (car dl)) (deep-foldr op nv term (cdr dl))))))
+
+(define (foldr op nv l)
+  (if (null? l) nv
+      (op (car l) (foldr op nv (cdr l)))))
+
+(define (deep-foldr op nv term dl)
+  (if (atom? dl) (term dl)
+      (foldr op nv (map (lambda (x) (deep-foldr op nv term x)) dl))))
+
+(define (evali x) (eval x (interaction-environment)))
