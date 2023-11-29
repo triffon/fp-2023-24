fe1c65d41788be28fd4e04d81bab8551ee55b76b
reo101 <pavel.atanasov2001@gmail.com>
2023-11-29 15:43:29 +0200

    feat(exercise/elective): add streams to `08-scheme-graphs-delay` `in_class.rkt`

--- exercises/elective/08-scheme-graphs-delay/in_class.rkt
+++ exercises/elective/08-scheme-graphs-delay/in_class.rkt
@@ -35,3 +35,77 @@
     (lambda (pair)
       (member x (cdr pair)))
     g))
+
+;;;;;;;;;;;;;;;
+;;; Streams ;;;
+;;;;;;;;;;;;;;;
+
+;; {{{ my-delay
+
+(define-syntax my-delay
+  (syntax-rules ()
+    ((_ nqkyv-kod)
+     (lambda ()
+       nqkyv-kod))))
+
+;; }}}
+
+;; {{{ my-force
+
+(define my-force
+  (lambda (promise)
+    (promise)))
+
+;; }}}
+
+;; {{{ my-stream-cons
+
+(define-syntax my-stream-cons
+  (syntax-rules ()
+    ((_ fst rst)
+     (cons fst (my-delay rst)))))
+
+;; }}}
+
+;; {{{ (my-stream-first s)
+
+(define (my-stream-first s)
+  (car s))
+
+;; }}}
+
+;; {{{ (my-stream-rest s)
+
+(define (my-stream-rest s)
+  (my-force (cdr s)))
+
+;; }}}
+
+;; {{{ (nats-helper n)
+
+(define (nats-from n)
+  (my-stream-cons
+   n
+   (nats-from (+ n 1))))
+
+;; }}}
+
+;; {{{ nats
+
+(define nats
+  (nats-from 0))
+
+;; }}}
+
+;; {{{ (stream-take s n)
+
+(define (stream-take s n)
+  (if (= n 0)
+      '()
+      (cons (my-stream-first s)
+            (stream-take (my-stream-rest s)
+                         (- n 1)))))
+
+;; }}}
+
+;; vim: set foldmethod=marker:
