7b2034874dbd753c97b4df057a21a04e24d20ee2
pvarna <peter.kolev2001@gmail.com>
2023-11-30 01:20:08 +0200

    add code snippets for Week 09

---
+++ exercises/inf2/Week09-ListsAndTuples/snippets/03-partialApplication.hs
@@ -0,0 +1,17 @@
+add :: Int -> Int -> Int
+add x y = x + y
+
+add5 :: Int -> Int
+add5 = add 5
+
+take3 :: [Int] -> [Int]
+take3 = take 3
+
+-- power :: Int -> Int -> Int
+-- power x y = x ^ y
+
+-- square :: Int -> Int
+-- square = power 2
+
+-- cube :: Int -> Int
+-- cube = power 3
