52dd6ced08fdd7abf29f674846752f53ff97cd45
Petar Kolev <petar.kolev@ocado.com>
2023-12-21 19:09:50 +0200

    add updated code snippet

--- exercises/inf2/Week12-AlgebraicDataTypes/snippets/02-ADSs.hs
+++ exercises/inf2/Week12-AlgebraicDataTypes/snippets/02-ADSs.hs
@@ -16,3 +16,17 @@ myTriangle = Triangle 5 2.2 4.33
 
 data BinaryTree a = EmptyTree | Node a (BinaryTree a) (BinaryTree a)
     deriving (Show)
+
+myCoolF :: (Num a) => Shape a -> String
+myCoolF (Circle _) = "Doing something cool with a circle"
+myCoolF (Triangle _ _ _) = "Doing something cool with a triangle"
+
+data Option a = Nishto | Neshto a
+
+data Greshka = DelenieNaNula | DrugaProstiq | Greshka10
+
+data Either a = Greshka a | Validno a
+
+g :: (Num a) -> a -> a -> Option a
+g _ 0 = Nishto
+g a b = Neshto (a `div` b)
