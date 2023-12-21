03567306c44296602861c6f3e3601932ccf9c921
Petar Kolev <petar.kolev@ocado.com>
2023-12-21 10:33:25 +0200

    add Week11

--- exercises/inf2/Week11-Typeclasses/snippets/01-typeclasses.hs
+++ exercises/inf2/Week11-Typeclasses/snippets/01-typeclasses.hs
@@ -9,9 +9,9 @@ quicksort (x:xs) = quicksort lower ++ [x] ++ quicksort higher
     higher = [y | y <- xs, y > x]
 
 
--- if only numbers then Num
--- if ==, /= then Eq
--- if <, <=, >=, > then Ord
--- if only Float and Double then Fractional
--- if only Int and Integer then Integral
--- if making an algebraic type that has to be printed then Show
+-- if only numbers, then Num
+-- if ==, /=, then Eq
+-- if <, <=, >=, >, then Ord
+-- if only Float and Double, then Fractional
+-- if only Int and Integer, then Integral
+-- if making an algebraic type that has to be printed, then Show
