598c5992c3e80c13996d978f75e4365757bdc28f
reo101 <pavel.atanasov2001@gmail.com>
2024-01-10 14:24:01 +0200

    feat(exercises/elective): add `13-haskell-monads-io-programs`

---
+++ exercises/elective/13-haskell-monads-io-programs/in_class.hs
@@ -0,0 +1,12 @@
+-- class Functor f where
+--   fmap :: (a -> b) -> f a -> f b
+--
+-- class Functor f => Applicative f where
+--   pure :: a -> f a
+--   (<*>) :: f (a -> b) -> f a -> f b
+--
+-- class Applicative m => Monad m where
+--   return :: a -> m a
+--   (>>=) :: m a -> (a -> m b) -> m b
+
+
